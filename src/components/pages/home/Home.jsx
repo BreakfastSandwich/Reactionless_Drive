4c5b5da04a3a0e69d24bd6ce25bb240b1e2b5bab
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-11-13 09:54:03 -0500

    home page complete with img

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import HomeCSS from './home.module.css'
+import ProfileIMG from '../../../assets/Profile.jpg'
 
 
 export default function Home() {
     return (
       <div className={HomeCSS.container}>
-        <header className={HomeCSS.header}>Will Speakman</header>
-        <p className={HomeCSS.body}>
+        <div className={HomeCSS.image}><img src={ProfileIMG} alt="" /></div>
+        <div className={HomeCSS.hero}>
+        <header className={HomeCSS.name}>Will Speakman</header>
+        <p className={HomeCSS.title}>
          Full Stack Web Developer
         </p>
+        </div>
+        
       </div>
     );
   }
